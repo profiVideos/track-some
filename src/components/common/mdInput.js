361ff574d226443c2dc797a65b0a5bfad27959da
Markus <mgriebling@profivideos.com>
2018-01-01 22:57:29 +0100

    Created a material design input class

---
+++ src/components/common/mdInput.js
@@ -0,0 +1,217 @@
+/**
+ * React Native Class
+ * mdInput - Material Design Input
+ * based on TextInput from ....
+ */
+
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import TextField from 'react-native-material-textinput';
+import AppColors from '../../templates/appColors';
+
+// ... make a component ...
+class mdInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      localVal: props.value   // ... make a copy of passed input var ... 
+    };
+  }
+
+  handleChange(text) {
+    this.setState({ localVal: text });
+    console.log(this.state.localVal);
+  }
+
+  render() {
+    return (
+      <View style={styles.containerStyle}>
+        <TextField
+          style={[styles.inputStyle, this.props.style]}
+          //label={this.props.label}
+          labelColor={AppColors.accentColor}
+          //placeholder={this.props.placeholder}
+          color={AppColors.hiliteColor}
+          //value={this.props.value}
+          //onChangeText={this.props.onChangeText}
+          paddingTop={16}
+          paddingBottom={2}
+          labelActiveColor={AppColors.accentColor}
+          labelActiveScale={0.75}
+          autoCorrect={false}
+          underlineActiveColor={AppColors.accentColor} 
+          underlineColorAndroid='transparent'
+          {...this.props}
+        />
+      </View>
+    );
+  }
+}
+
+export default mdInput;
+
+/*
+const AppColors = {
+  hiliteColor: '#fff8b2',     // ... light yellow ...
+  accentColor: '#dea140',     // ... medium orange ...
+  mainLiteColor: '#a32b26',   // ... medium red ...
+  mainDarkColor: '#590d0b',   // ... dark red (burgundy) ...
+  darkerColor: '#325a66'      // ... dark cyan ....
+*/
+
+const styles = StyleSheet.create({
+  containerStyle: {
+    width: '80%',
+    paddingLeft: 4,
+    paddingRight: 4,
+    borderRadius: 5,
+    borderColor: '#929292',
+    borderWidth: 0,
+    shadowColor: '#121212',
+    shadowOffset: { width: 1, height: 3 },
+    shadowOpacity: 0.85,
+    elevation: 3,
+    opacity: 1.0,
+    backgroundColor: '#282828',
+    marginBottom: 8
+    //backgroundColor: AppColors.mainDarkColor
+  },
+  inputStyle: {
+    color: '#f2f2f2',
+    fontSize: 28,
+    fontWeight: '400',
+    width: '100%',
+    height: 40,
+    shadowColor: '#121212',
+    shadowOffset: { width: 1, height: 3 },
+    shadowOpacity: 0.85,
+    opacity: 1.0
+  }
+});
+
+/*
+
+          <DefaultInput
+            placeholder="Place Name"
+            value={this.state.emailAddr}
+            valid
+            touched={false}
+            onChangeText={emailAddr => this.setState({ emailAddr })}
+          />
+
+
+  <TextInput
+    underlineColorAndroid="transparent"
+    {...props}
+    style={[styles.input, props.style, 
+      !props.valid && props.touched ? styles.invalid : null]}
+  />
+
+        <TextField
+          style={[styles.inputStyle, this.props.style]}
+          value={this.state.inputVal}
+          onChangeText={text => this.handleChange(text)}
+          autoCorrect={false}
+          underlineColorAndroid="transparent"
+          {...this.props}
+        />
+
+*/
+
+
+  /*
+      paddingRight: 5,
+      paddingLeft: 5,
+      paddingBottom: 2,
+
+          placeholder={this.props.placeholder}
+          secureTextEntry={this.props.secureTextEntry}
+
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 3 },
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position: 'relative',
+    backgroundColor: '#434265'
+  },
+  headerStyle: {
+    fontSize: 22,
+    color: '#F8e033'
+
+  input: {
+    color: '#111111',
+    //marginTop: 5,
+    //padding: 3,
+    width: '80%',
+    //height: 35,
+    //borderWidth: 2,
+    //backgroundColor: 'white',
+    //opacity: 0.5,
+  },
+  backgroundImage: {
+    flex: 1,
+    borderColor: 'white',
+    borderWidth: 1
+  },
+  buttonStyle: {
+    marginTop: 15
+  },
+  card1: {
+    width: '80%',
+  },
+  title: {
+    paddingBottom: 16,
+    textAlign: 'center',
+    color: '#ffffff',
+    fontSize: 20,
+    fontWeight: 'bold',
+    opacity: 0.8,
+  },
+  logoPortrait: {
+    marginTop: 20,
+    width: 218,
+    height: 197
+  },
+  logoLandscape: {
+    marginTop: 5,
+    width: 132,
+    height: 120
+  },
+  logoContainer: {
+    flex: 2,
+    alignItems: 'center'
+  },
+  container: {
+    flex: 3,
+    alignItems: 'center'
+  },
+  text: {
+    color: '#f2f2f2',
+    margin: 10
+  },
+  inputStyle: {
+      paddingRight: 5,
+      paddingLeft: 5,
+      paddingBottom: 2,
+      color: '#262626',
+      fontSize: 18,
+      fontWeight: '200',
+      flex: 1,
+      width: '100%',
+      height: 40
+  },
+  labelStyle: {
+      fontSize: 12,
+      color: '#7F7D7D',
+      fontWeight: '200',
+      flex: 1
+  },
+  containerStyle: {
+      height: 60,
+      padding: 5,
+      flexDirection: 'column',
+      alignItems: 'flex-start',
+      borderColor: '#D4D4D4',
+      borderBottomWidth: 1,
+  }
+  */
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
 import { 
   View, 
-  Text, 
+  Text,
   Button, 
   Image,
+  ScrollView,
   StyleSheet,
   Dimensions, 
   ImageBackground } from 'react-native';
 
-import Icon from 'react-native-vector-icons/FontAwesome';
+//import { Hideo } from 'react-native-textinput-effects';
+//import Icon from 'react-native-vector-icons/FontAwesome';
+
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import tracksomeLogo from '../images/tracksome-logo-684w.png';
+import MDInput from '../components/common/mdInput';
+//import DefaultInput from '../components/common/DefaultInput';
 import splashBackgroundImage from '../images/login-splash-screen-1080w.jpg';
+import AppColors from '../templates/appColors';
 
 //import loadMainTabs from './loadMainTabs';
 
@@ -29,37 +36,90 @@ class Login extends Component {
 
   state = {
     toggled: false,
+    emailAddr: 'markus@profiphotos.com',
     scrWidth: Dimensions.get('window').width,
     scrHeight: Dimensions.get('window').height,
     viewMode: this.scrHeight > this.scrWidth ? 'portrait' : 'landscape'
   }
   
   loginHandler = () => {
-      this.alert('Screen Height:', this.state.scrHeigh, '  Orientation: ', this.state.viewMode);
+      alert('Screen Height:' + this.state.scrHeight + '  Orientation: ' + 
+        this.state.viewMode);
+      //loadMainTabs();
+  }
+
+  signupHandler = () => {
+      alert('Screen Height:' + this.state.scrHeight + '  Orientation: ' + 
+        this.state.viewMode);
+      //loadMainTabs();
+  }
+
+  testDriveHandler = () => {
+      alert('Going to the fun part.  Screen Height:' + this.state.scrHeight + '  Orientation: ' + 
+        this.state.viewMode);
       //loadMainTabs();
   }
 
   loginWithFacebook = () => {
-      this.alert('Pressed the Facebook button');
+      alert('Pressed the Facebook button');
       //loadMainTabs();
+  }
+
+  handleTextChange(text) {
+    this.setState({ emailAddr: text });
+    console.log(this.state.emailAddr);
   }
 
   render() {
     return (
       <ImageBackground source={splashBackgroundImage} style={styles.backgroundImage}>
-        <View style={styles.logoContainer}>
-          <Image 
-            source={tracksomeLogo} 
-            style={this.state.viewMode === 'portrait' ? styles.logoPortrait : styles.logoLandscape} 
-          />
-        </View>
-        <View style={styles.container}>
-          <Text style={styles.text}>Login Screen</Text>
-          <Button title="Login" onPress={this.loginHandler} />
-          <Icon.Button name="facebook" backgroundColor="#3b5998" onPress={this.loginWithFacebook}>
-            Login with Facebook
-          </Icon.Button>
-        </View>
+        <ScrollView style={{ flex: 1 }}>
+          <View style={styles.logoContainer}>
+            <Image 
+              source={tracksomeLogo} 
+              style={this.state.viewMode === 'portrait' ? 
+                styles.logoPortrait : styles.logoLandscape} 
+            />
+          </View>
+          <View style={styles.container}>
+            <Text style={styles.title}>Login / Signup</Text>
+            <MDInput 
+              label='Email'
+              placeholder='Your Email Address please ... '
+              keyboardType='email-address'
+              value={this.state.emailAddr}
+              onChangeText={value => this.handleTextChange(value)}
+              validEmailCheck
+            />
+            <MDInput 
+              label='Password'
+              placeholder='Password please (min. 8 chars) ... '
+              secureTextEntry
+              //value={this.state.password}
+            />
+            <MDInput 
+              label='Confirm Password'
+              placeholder='Again again please (to verify) ... '
+              //secureTextEntry
+              //value={this.state.password}
+            />
+            <View style={styles.buttonRow}>
+              <Button style={styles.buttonStyle} title="Login" onPress={this.loginHandler} />
+              <Button style={styles.buttonStyle} title="Signup" onPress={this.signupHandler} />
+            </View>
+            <Text style={styles.text}>
+            If you've never signed up before, there are some benefits to doing so.  
+            Or you could just take this app for a test drive by clicking the button below.
+            </Text>
+          </View>
+          <View style={{ flex: 1 }}>
+              <Button 
+                style={styles.funStyle} 
+                title="Take a Test Drive (no Saving)" 
+                onPress={this.testDriveHandler} 
+              />
+          </View>
+        </ScrollView>
       </ImageBackground>
     );
   }
@@ -67,17 +127,58 @@ class Login extends Component {
 }
 
 /*
-          <Switch 
-            onValueChange={ (value) => this.setState({ toggled: value })} 
-            value={ this.state.toggled } 
-          /> 
+            style={styles.input}
+            label='Email'
+            placeholder='Your Email Address'
+            value={this.state.emailAddr}
+            maxLength={20}
+            error='You must enter a valid eMail-Address!'
+            onChangeText={emailAddr => this.setState({ emailAddr })}
+*/
+
+/*
+const AppColors = {
+  hiliteColor: '#fff8b2',     // ... light yellow ...
+  accentColor: '#dea140',     // ... medium orange ...
+  mainLiteColor: '#a32b26',   // ... medium red ...
+  mainDarkColor: '#590d0b',   // ... dark red (burgundy) ...
+  darkerColor: '#325a66'      // ... dark cyan ....
 */
 
 const styles = StyleSheet.create({
+  input: {
+    color: '#111111',
+    width: '80%'
+  },
   backgroundImage: {
+    flex: 1
+  },
+  buttonRow: {
     flex: 1,
-    borderColor: 'white',
-    borderWidth: 1
+    width: '50%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  buttonStyle: {
+    margin: 15
+  },
+  funStyle: {
+    width: '80%',
+    color: AppColors.mainDarkColor,
+    backgroundColor: AppColors.accentColor
+  },
+  title: {
+    paddingTop: 10,
+    paddingBottom: 10,
+    textAlign: 'center',
+    color: '#f2f2f2',
+    fontSize: 20,
+    fontWeight: '700',
+    shadowColor: '#121212',
+    shadowOffset: { width: 1, height: 3 },
+    shadowOpacity: 0.85,
+    elevation: 2,
   },
   logoPortrait: {
     marginTop: 20,
@@ -99,8 +200,19 @@ const styles = StyleSheet.create({
   },
   text: {
     color: '#f2f2f2',
-    margin: 10
+    margin: 20,
+    textAlign: 'center'
   }
 });
+
+/*
+          <Switch 
+            onValueChange={ (value) => this.setState({ toggled: value })} 
+            value={ this.state.toggled } 
+          /> 
+          <Icon.Button name="facebook" backgroundColor="#3b5998" onPress={this.loginWithFacebook}>
+            Login with Facebook
+          </Icon.Button>
+*/
 
 export default Login;
